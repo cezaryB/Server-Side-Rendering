59e20b4b0f15f07437f556dbebdb0f42590e6751
Cezary <czarekburzykowski@gmail.com>
2017-12-18 21:43:45 +0100

    rendering the test component

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const React = require('react')
+const renderToString = require('react-dom/server').renderToString
+const Home = require('./client/components/Home').default
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res, next) => {
+  const content = renderToString(<Home />)
+  res.send(content)
 
 })
 
